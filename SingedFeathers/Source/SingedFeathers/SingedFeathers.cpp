4de657d9f4ae44f663000d76d8910e00593ee40f
Caden Marofke <spiritaflame@gmail.com>
2017-01-31 21:30:09 -0600

    Add remainder of Unreal project files
    Moved gitignore file, hopefully added only the needed project files for
    the Unreal project.

---
+++ SingedFeathers/Source/SingedFeathers/SingedFeathers.cpp
@@ -0,0 +1,6 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#include "SingedFeathers.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SingedFeathers, "SingedFeathers" );
+ 
