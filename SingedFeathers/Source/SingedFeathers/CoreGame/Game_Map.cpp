4d7bab1abfbebc6758e87fd11bf4c1100f95f569
MuCephei <garrettsuss@gmail.com>
2017-02-05 17:22:51 -0600

    Map is now rotated correctly

--- SingedFeathers/Source/SingedFeathers/CoreGame/Game_Map.cpp
+++ SingedFeathers/Source/SingedFeathers/CoreGame/Game_Map.cpp
@@ -2,15 +2,16 @@
 
 #include "SingedFeathers.h"
 #include <string>
+#include "Tile_Info.cpp"
 #include "ConstructorHelpers.h"
 #include "Game_Map.h"
 
 // Sets default values
 AGame_Map::AGame_Map(/*const FObjectInitializer&*/)
 {
-    height = 2;
-    width = 2;
-    tileMap = vector<vector<int>>(height,vector<int>(width,2));
+    height = 4;
+    width = 4;
+    tileMap = vector<vector<int>>(height,vector<int>(width, tileType::ash));
     tileMap.at(0).assign(0,3);
     UE_LOG(LogTemp, Warning, TEXT("Init map"));
     //load this in later
@@ -38,7 +39,7 @@ void AGame_Map::AddBaseTile(UBlueprint* tile) {
 }
 
 ABase_Tile* AGame_Map::GetBaseTile(int x, int y) {
-    UE_LOG(LogTemp, Warning, TEXT("Made Base Tile"));
+    UE_LOG(LogTemp, Warning, TEXT("Made Base Tile x:%d y:%d"), x, y);
     FVector Location(x * tilePixels, y * tilePixels, 0.0f);
     FRotator Rotation(90.0f, 0.0f, 0.0f);
     ABase_Tile* baseTile = GetWorld()->SpawnActor<ABase_Tile>(Location, Rotation);
@@ -47,6 +48,8 @@ ABase_Tile* AGame_Map::GetBaseTile(int x, int y) {
 }
 
 FVector AGame_Map::GetMapLocation(int x, int y) {
+    FVector result(x * tilePixels, y * tilePixels, 0.0f);
+    UE_LOG(LogTemp, Warning, TEXT("Made Base Tile x:%f, y:%f, z:%f"), result.X, result.Y, result.Z);
     return FVector(x * tilePixels, y * tilePixels, 0.0f);
 }
 
