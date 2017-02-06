f493009694b8e63ef8fdffa08bb0c1036d830b96
Caden Marofke <spiritaflame@gmail.com>
2017-02-06 11:57:40 -0600

    Remove test project from Develop

--- SingedFeathers/Source/SingedFeathers/SingedFeathers.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
-
-#include "SingedFeathers.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SingedFeathers, "SingedFeathers" );
- 
