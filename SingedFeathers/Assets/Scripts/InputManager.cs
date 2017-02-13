033d1429431deeb3b59ba7b5fad6ae65eed34449
Caden Marofke <spiritaflame@gmail.com>
2017-02-13 01:31:47 -0600

    Create Unity Project
    - created Unity project
    - removed Unreal project
    - migrated Unreal code to Unity
    - added Title, Game, and Map Maker scenes
    - added simple UI to navigate scenes
    - added ability to load maps from 2D int arrays

---
+++ SingedFeathers/Assets/Scripts/InputManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+
+		#if !MOBILE_INPUT
+		if (Input.GetMouseButtonDown (0)) {
+			Vector2 worldPoint = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			RaycastHit2D hit = Physics2D.Raycast (worldPoint, Vector2.zero);
+
+			if (hit.collider != null) {
+				if (hit.transform.gameObject.tag == "Tile") {
+					Debug.Log (hit.transform.gameObject.GetComponent <TileInfo>().type);
+					hit.transform.gameObject.GetComponent <TileInfo>().ApplyHeat (100);
+				} 
+			} 
+		}
+		#endif
+		//TODO: add mobile input 
+	}
+}
---
+++ SingedFeathers/Assets/Scripts/TileInfo.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileInfo : MonoBehaviour {
+
+	//TODO: have a place to put constants
+	const int BURN_HEAT = 5;
+
+	//set these in the editor for now
+	public bool _isFlammable;
+	public bool _isBurntOut;
+	public int _flashPoint;
+	public int _durability;
+	public int _burnDuration;
+	public TileType type;
+
+	int _heatThisTurn;
+	bool _onFire;
+	bool _shouldSpreadFireThisTurn;
+	List<TileInfo> _neighbouringTiles;
+
+	// Use this for initialization
+	void Awake () {
+		_heatThisTurn = 0;
+		_onFire = false;
+		_shouldSpreadFireThisTurn = false;
+		_neighbouringTiles = new List<TileInfo> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void StartTurn() {
+		_heatThisTurn = 0;
+		_shouldSpreadFireThisTurn = _onFire;
+	}
+
+	public void SpreadFire() {
+		if (_shouldSpreadFireThisTurn) {
+			foreach (TileInfo neighbour in _neighbouringTiles) {
+				neighbour.ApplyHeat(BURN_HEAT);
+			}
+			_burnDuration--;
+			if (_burnDuration <= 0) {
+				_isBurntOut = true;
+			}
+		}
+	}
+
+	public void AddTileToNeighbours(TileInfo tile) {
+		_neighbouringTiles.Add (tile);
+	}
+
+	public void SetOnFire() {
+		_onFire = true;
+		//TODO: load/create a Fire object here
+	}
+
+	public void ApplyHeat(int heat) {
+		if (_isFlammable && !_onFire) {
+			_heatThisTurn += heat;
+			if (_heatThisTurn >= _flashPoint) {
+				SetOnFire();
+			} else {
+				_durability -= heat;
+				if (_durability <= 0) {
+					SetOnFire();
+				}
+			}
+		}
+	}
+
+	public bool IsBurntOut() { return _isBurntOut; }
+}
