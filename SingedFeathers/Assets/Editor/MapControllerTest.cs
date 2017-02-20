3803f7ff387b2b135f061768215db482e28e3393
Nicole Epp <nicole.epp@invenia.ca>
2017-02-20 14:35:39 -0600

    Add initial unit tests
    Tests were created by Jill

---
+++ SingedFeathers/Assets/Editor/MapControllerTest.cs
@@ -0,0 +1,21 @@
+﻿using Assets.Scripts.Controllers;
+using NUnit.Framework;
+
+namespace SingedFeathers.Test.ControllerTest {
+    [TestFixture()]
+    public class MapControllerTest {
+        private MapController _mapController;
+
+        [SetUp]
+        public void Init() {
+            _mapController = new MapController();
+        }
+
+        [Test]
+        public void Generate_InitializesProperly() {
+            _mapController.GenerateMap();
+            Assert.AreEqual(5, _mapController.Height);
+            Assert.AreEqual(5, _mapController.Width);
+        }
+    }
+}
