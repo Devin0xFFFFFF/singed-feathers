bf7409830b54bf001bc0786c4f271123e53d93d4
radix <hicksonj@myumanitoba.ca>
2017-02-23 20:58:29 -0600

    add unity test tools; add initial mocked setup for MapController tests

---
+++ SingedFeathers/Assets/Editor/ControllerTests/MapControllerTest.cs
@@ -0,0 +1,58 @@
+﻿using Assets.Scripts.Controllers;
+using Assets.Scripts.Models;
+using Assets.Scripts.Service;
+using NUnit.Framework;
+using NSubstitute;
+
+namespace Assets.Editor.ControllerTests {
+    [TestFixture()]
+    public class MapControllerTest {
+        private MapController _mapController;
+        private ITileController _tile1;
+        private ITileController _tile2;
+        private ITileController _tile3;
+
+        [SetUp]
+        public void Init() {
+            IMapGeneratorService mapGenerator = Substitute.For<IMapGeneratorService>();
+            mapGenerator.GenerateMap(Arg.Any<int>()).Returns(GenerateTestMap());
+            _mapController = new MapController(mapGenerator);
+        }
+
+        [Test]
+        public void TestGenerateInitializesProperly() {
+            _mapController.GenerateMap();
+            Assert.AreEqual(3, _mapController.Height);
+            Assert.AreEqual(1, _mapController.Width);
+        }
+
+        [Test]
+        public void TestApplyHeatDoesNotThrowOutOFBoundsException() {
+            _mapController.ApplyHeat(-1, -1);
+            _mapController.ApplyHeat(-1, 0);
+            _mapController.ApplyHeat(0, -1);
+
+        }
+
+        private Map GenerateTestMap() {
+            ITileController[,] tileControllers = IntializeControllers();
+            return new Map() {
+                Height = 3,
+                Width = 1,
+                InitialFirePosition = new Position() { X = 1, Y = 0 },
+                TileMap = tileControllers
+            };
+        }
+
+        private ITileController[,] IntializeControllers() {
+            _tile1 = Substitute.For<ITileController>();
+            _tile2 = Substitute.For<ITileController>();
+            _tile3 = Substitute.For<ITileController>();
+
+            ITileController[,] tileControllers = {
+                { _tile1, _tile2, _tile3 }
+            };
+            return tileControllers;
+        }
+    }
+}
---
+++ SingedFeathers/Assets/Editor/ServiceTests/MapGeneratorServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Assets.Scripts.Service;
+using NUnit.Framework;
+
+namespace Assets.Editor.ServiceTests {
+    [TestFixture]
+    public class MapGeneratorServiceTest {
+        private MapGeneratorService _mapGeneratorService;
+
+        [SetUp]
+        public void Init() {
+            _mapGeneratorService = new MapGeneratorService();
+        }
+
+        public void TestMapGeneration() {
+            _mapGeneratorService.GenerateMap(1);
+            //...do more things... is this one even testable?
+        }
+    }
+}
