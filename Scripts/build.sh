0ebe88b84cf16a25d4d9ac62f636c001f8efa486
Nicole Epp <nicole.epp@invenia.ca>
2017-02-28 18:24:01 -0600

    Add webgl build step

---
+++ Scripts/build.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+
+project="SingedFeathers"
+
+echo "Attempting to build $project for Webgl"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile $(pwd)/SingedFeathers/Unity.log \
+  -projectPath=$(pwd)/SingedFeathers \
+  -buildTarget webgl
+  -quit
+
+echo 'Logs from build'
+cat $(pwd)/SingedFeathers/Unity.log
+
+
+#echo 'Attempting to zip builds'å
+#zip -r $(pwd)/Build/linux.zip $(pwd)/Build/linux/
+#zip -r $(pwd)/SingedFeathers/Build/mac.zip . -i $(pwd)/SingedFeathers/Build/osx/
+#zip -r $(pwd)/Build/windows.zip $(pwd)/Build/windows/
