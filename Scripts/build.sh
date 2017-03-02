d2f30d34785e5be7065b1771430523ab521924a4
Nicole Epp <nicole.epp@invenia.ca>
2017-03-01 18:04:51 -0600

    Update build script

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -8,7 +8,6 @@ echo "Attempting to build $project for Webgl"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
   -nographics \
-  -silent-crashes \
   -logFile $(pwd)/Unity.log \
   -projectPath=$(pwd)/SingedFeathers \
   -executeMethod Assets.Editor.Build.AutoBuilder.PerformWebBuild \
@@ -20,7 +19,3 @@ echo 'Logs from build'
 cat $(pwd)/SingedFeathers/Unity.log
 
 
-#echo 'Attempting to zip builds'å
-#zip -r $(pwd)/Build/linux.zip $(pwd)/Build/linux/
-#zip -r $(pwd)/SingedFeathers/Build/mac.zip . -i $(pwd)/SingedFeathers/Build/osx/
-#zip -r $(pwd)/Build/windows.zip $(pwd)/Build/windows/
