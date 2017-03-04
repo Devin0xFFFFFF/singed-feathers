4e1fa23d3cbb53bb2ee77ab9754d905c45b2f8a4
radix <hicksonj@myumanitoba.ca>
2017-03-04 16:45:32 -0600

    Expose internal Tile and Pigeon object for the purpose of serialization

---
+++ SingedFeathers/Assets/Scripts/Models/Pigeon.cs
@@ -0,0 +1,8 @@
+﻿namespace Assets.Scripts.Models {
+    public class Pigeon {
+        public int Health;
+        private const int INITIAL_HEALTH = 100;
+
+        public Pigeon() { Health = INITIAL_HEALTH; }
+    }
+}
