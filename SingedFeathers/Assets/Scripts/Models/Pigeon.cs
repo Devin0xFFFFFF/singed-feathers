0d9c93eee8752ce1aa5b14781e73f90f89d34886
radix <hicksonj@myumanitoba.ca>
2017-03-07 13:55:00 -0600

    update

--- SingedFeathers/Assets/Scripts/Models/Pigeon.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Assets.Scripts.Models {
-    public class Pigeon {
-        public int Health;
-        private const int INITIAL_HEALTH = 100;
-
-        public Pigeon() { Health = INITIAL_HEALTH; }
-    }
-}
