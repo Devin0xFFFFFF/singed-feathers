53168dc7779093c00286044c426fdc693a553e52
SashaG-T <sasha.gervaistourangeau@gmail.com>
2017-03-12 15:42:14 -0500

    Cleaned up for review.

--- SingedFeathers/Assets/Scripts/Views/TileView.cs
+++ SingedFeathers/Assets/Scripts/Views/TileView.cs
@@ -12,24 +12,24 @@ namespace Assets.Scripts.Views {
         private Sprite[] _heatLevelSprites;
         private ITileController _tileController;
         private SpriteRenderer _spriteRenderer;
-        private const int HEAT_PER_INDEX = 5;
+        private const int HEAT_PER_INDEX = 10;
+        private int _maxIndex;
 
         public void Start() {
             _heatLevelSprites = new Sprite[] { SpriteHeatLevel0, SpriteHeatLevel1, SpriteHeatLevel2, SpriteHeatLevel3 };
             _spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+            _maxIndex = _heatLevelSprites.Length - 1;
         }
-
 
         // Update is called once per frame
         public void Update() {
             Transform child = transform.GetChild(0);
+            int spriteIndex = _tileController.GetTileHeat() / HEAT_PER_INDEX;
             child.gameObject.SetActive(_tileController.IsOnFire());
-           // if (_tileController.StateHasChanged) {
-                int spriteIndex = _tileController.GetTileHeat() / HEAT_PER_INDEX;
-                if (spriteIndex < _heatLevelSprites.Length) {
-                    _spriteRenderer.sprite = _heatLevelSprites[spriteIndex];
-                }
-           // }
+            if (spriteIndex >= _maxIndex) {
+                spriteIndex = _maxIndex;
+            }
+            _spriteRenderer.sprite = _heatLevelSprites[spriteIndex];
         }
 			
         public void ApplyHeat(int heat) { _tileController.ApplyHeat(heat); }
