fb665dabf179e5a0f38ffc4473a95d96d2002c19
Sam Morrison <morris25@myumanitoba.ca>
2017-03-24 11:34:18 -0500

    spacing changes

--- SingedFeathers/Assets/Scripts/Input/MapSelectionInput.cs
+++ SingedFeathers/Assets/Scripts/Input/MapSelectionInput.cs
@@ -1,15 +1,13 @@
-﻿using CoreGame.Models.API.MapClient;
-using UnityEngine;
+﻿using UnityEngine;
 using Assets.Scripts.Service;
-using System.Collections;
+using CoreGame.Models.API.MapClient;
 using System.Collections.Generic;
-using Newtonsoft.Json;
 using UnityEngine.UI;
 
 namespace Assets.Scripts.Input {
     public class MapSelectionInput : MonoBehaviour {
-        private MapIO _mapIO;
         public GameObject MapSelectButton;
+        private MapIO _mapIO;
 
         public void Start() {
             _mapIO = new MapIO ();
@@ -25,18 +23,15 @@ namespace Assets.Scripts.Input {
                 foreach(MapInfo map in maps){
                     GameObject mapButton = Instantiate(MapSelectButton);
                     Button tempButton = mapButton.GetComponent<Button>();
-                    tempButton.GetComponentInChildren<Text>().text = "Map Name: "+map.MapName+"\t Creator: "
-                        +map.CreatorName+"\n Play Type: " + map.MapType;
+                    tempButton.GetComponentInChildren<Text>().text = "Map Name: "+ map.MapName +"\t Creator: "
+                        + map.CreatorName + "\n Play Type: " + map.MapType;
                     tempButton.onClick.AddListener(delegate {SelectMap(map.MapID);});
 
                     mapButton.transform.SetParent( this.GetComponent<RectTransform>());
                 }
-
-            
             }));
         }
+
         public void SelectMap(string MapID) { PlayerPrefs.SetString ("MapID", MapID); }
-
     }
-
 }
