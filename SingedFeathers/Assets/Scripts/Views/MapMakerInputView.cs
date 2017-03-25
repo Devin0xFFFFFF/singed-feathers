8e907aa55b3da67c29e852a0b927ec0fc7d7507c
Caden Marofke <spiritaflame@gmail.com>
2017-03-25 14:23:54 -0500

    Add tile selection UI

--- SingedFeathers/Assets/Scripts/Views/MapMakerInputView.cs
+++ SingedFeathers/Assets/Scripts/Views/MapMakerInputView.cs
@@ -8,17 +8,34 @@ namespace Assets.Scripts.Views {
 
         public Button AddFireButton;
         public Button AddPigeonButton;
-        public GameObject TileSet;
+        public Button TileButtonBase;
+        public GameObject TileButtonContainer;
+        private List<TileView> _tileSet;
         private MapMakerView _mapMakerView;
 
         void Start() {
             _mapMakerView = GetComponent<MapMakerView>();
+            _tileSet = _mapMakerView.TileSet;
+            InitializeTileButtons();
+        }
+
+        void Update() {
+
+                 
         }
 
         public override void HandleMapInput(TileView tileManager) { 
             Vector3 position = tileManager.gameObject.transform.position;
 
             //based on selection, do stuff to the tile
+        }
+
+        private void InitializeTileButtons() {
+            foreach(TileView tile in _tileSet) {
+                Button newButton = Instantiate(TileButtonBase);
+                newButton.GetComponent<Image>().sprite = tile.GetComponent<SpriteRenderer>().sprite;
+                newButton.transform.SetParent(TileButtonContainer.GetComponent<RectTransform>());
+            }
         }
     }
 }
