845e42fcd6d2f3409c3d0ebb7da143f260d3e53d
MuCephei <garrettsuss@gmail.com>
2017-03-27 22:20:08 -0500

    For sharing

--- SingedFeathers/Assets/Scripts/Controllers/WebTurnResolver.cs
+++ SingedFeathers/Assets/Scripts/Controllers/WebTurnResolver.cs
@@ -4,26 +4,24 @@ using UnityEngine;
 using System.Collections;
 using CoreGame.Controllers.Interfaces;
 using CoreGame.Models;
-using CoreGame.Models.Commands;
 using CoreGame.Utility;
 
-using System.IO;
 using CoreGame.Models.API.GameService;
+using Assets.Scripts.Service;
 
 namespace Assets.Scripts.Controllers {
     public class WebTurnResolver : MonoBehaviour, ITurnResolver {
         private bool _isTurnResolved = true;
         private bool _receivedResponse = true;
         private readonly JsonSerializerSettings _settings;
-        private string JsonDelta;
+        private GameServiceIO _gameServiceIO;
 
         public WebTurnResolver() {
             _settings = new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All };
         }
 
         public void Awake() {
-            Random.InitState(3);
-            //Three is the most random number
+            _gameServiceIO = new GameServiceIO();
         }
 
         public bool IsTurnResolved() { return _isTurnResolved; }
@@ -39,60 +37,28 @@ namespace Assets.Scripts.Controllers {
         public void Poll(Map map, Player player) {
             _receivedResponse = true;
             PollRequest request = new PollRequest("Test", player.PlayerID);
-            StartCoroutine(ExecuteAfterTime(0.1f, map));
+            SendPollRequest(request, map);
         }
 
         private IEnumerator ExecuteAfterTime(float time, Map map) {
             yield return new WaitForSeconds(time);
-
-            Delta translatedDelta = JsonConvert.DeserializeObject<Delta>(JsonDelta, _settings);
-
-            List<Delta> deltaList = new List<Delta>();
-            if (translatedDelta != null) {
-                deltaList.Add(translatedDelta);
-            }
-            bool success = Random.Range(0, 2) == 0;
-            PollResponse response = new PollResponse(success, deltaList);
-            _receivedResponse = response.IsValid;
-            if (response.IsValid) {
-                TurnResolveUtility.ApplyDelta(response.Turn, map);
-                _isTurnResolved = true;
-            }
         }
 
         private void SendCommitTurnRequest(CommitTurnRequest request) {
             _receivedResponse = false;
-            StartCoroutine(_mapIO.GetMapData(mapID, delegate (string serializedMapData) {
-                if (serializedMapData == null) {
-                    Debug.LogError("Failed to retrieve map.");
-                    return;
-                }
-                _mapController = new MapController();
-                if (!_mapController.GenerateMap(serializedMapData)) {
-                    Debug.LogError("Failed to generate map.");
-                    return;
-                }
-                _mapController.SetTurnResolver(TurnResolver);
 
-                _width = _mapController.Width;
-                _height = _mapController.Height;
-                _map = new TileView[_width, _height];
-
-                _tileSizeX = TileSet[0].GetComponent<Renderer>().bounds.size.x;
-                _tileSizeY = TileSet[0].GetComponent<Renderer>().bounds.size.y;
-
-                InstantiateTiles();
-                SetPlayerSideSelection();
-                SetPlayerSideSelectionText();
-                Debug.Log(_mapController.GetPlayerSideSelection());
-
-                LoadPigeons();
-                LoadInputView();
+            StartCoroutine(_gameServiceIO.CommitTurn(request, delegate (bool success) {
             }));
         }
 
-        private void SendPollRequest(PollRequest request) {
-            _receivedResponse = false;
+        private void SendPollRequest(PollRequest request, Map map) {
+            StartCoroutine(_gameServiceIO.PollGame(request, delegate (PollResponse response) {
+                _receivedResponse = response.IsValid;
+                if (response.IsValid) {
+                    TurnResolveUtility.ApplyDelta(response.Turn, map);
+                    _isTurnResolved = true;
+                }
+            }));
         }
     }
 }
---
+++ SingedFeathers/Assets/Scripts/Service/GameServiceClient.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine.Networking;
+using CoreGame.Models.API.GameService;
+
+namespace Assets.Scripts.Service {
+    public class GameServiceClient : APersistenceClient {
+        private const string COMMIT_TURN_PATH = "CommitTurn";
+        private const string POLL_GAME_PATH = "PollGame";
+
+        public GameServiceClient(AWSAPIClientConfig apiConfig = null) : base(apiConfig) { }
+
+        public IEnumerator CommitTurn(CommitTurnRequest commitTurnRequest, ResultCallback resultCallback) {
+            UnityWebRequest request = _requestBuilder.BuildPutRequest(COMMIT_TURN_PATH, SerializeObject(commitTurnRequest));
+            yield return request.Send();
+            ReturnResult(resultCallback, COMMIT_TURN_PATH, request);
+        }
+
+        public IEnumerator PollGame(PollRequest pollRequest, ResultCallback resultCallback) {
+            UnityWebRequest request = _requestBuilder.BuildPutRequest(POLL_GAME_PATH, SerializeObject(pollRequest));
+            yield return request.Send();
+            ReturnResult(resultCallback, POLL_GAME_PATH, request);
+        }
+    }
+}
