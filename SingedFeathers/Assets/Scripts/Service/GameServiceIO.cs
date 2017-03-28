41e6471889132c0df48f399160ac2b3c6ce21d75
MuCephei <garrettsuss@gmail.com>
2017-03-28 16:53:27 -0500

    Added Surrender as an option

--- SingedFeathers/Assets/Scripts/Service/GameServiceIO.cs
+++ SingedFeathers/Assets/Scripts/Service/GameServiceIO.cs
@@ -14,7 +14,6 @@ namespace Assets.Scripts.Service {
 
         public IEnumerator CommitTurn(CommitTurnRequest commitTurnRequest, CommitTurnCallback callback) {
             yield return _client.CommitTurn(commitTurnRequest, delegate (ClientResult result) {
-                System.IO.File.WriteAllText("Commit.txt", result.ResponseCode.ToString());
                 if (IsValidResult(result)) {
                     bool success = JsonConvert.DeserializeObject<bool>(result.ResponseBody);
                     Console.WriteLine("Response Recived " + success);
@@ -28,7 +27,19 @@ namespace Assets.Scripts.Service {
 
         public IEnumerator PollGame(PollRequest pollRequest, PollGameCallback callback) {
             yield return _client.PollGame(pollRequest, delegate (ClientResult result) {
-                System.IO.File.WriteAllText("Poll.txt", result.ResponseCode.ToString());
+                if (IsValidResult(result)) {
+                    PollResponse response = JsonConvert.DeserializeObject<PollResponse>(result.ResponseBody);
+                    Console.WriteLine("Response Recived " + response);
+                    callback(response);
+                } else {
+                    Console.WriteLine("Failed to poll game: " + result.ErrorMessage ?? result.ResponseCode + " " + result.ResponseBody);
+                    callback(new PollResponse(false, null));
+                }
+            });
+        }
+
+        public IEnumerator Surrender(PollRequest pollRequest, PollGameCallback callback) {
+            yield return _client.Surrender(pollRequest, delegate (ClientResult result) {
                 if (IsValidResult(result)) {
                     PollResponse response = JsonConvert.DeserializeObject<PollResponse>(result.ResponseBody);
                     Console.WriteLine("Response Recived " + response);
