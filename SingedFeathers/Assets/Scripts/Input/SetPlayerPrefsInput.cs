82513a55740d1f85ca1ebe9bb4f67973e04886e9
Sam Morrison <morris25@myumanitoba.ca>
2017-03-30 20:56:22 -0500

    lazy playerprefs script created
    singleplayer and multiplayer buttons should now set some lobby
    conditions

---
+++ SingedFeathers/Assets/Scripts/Input/SetPlayerPrefsInput.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CoreGame.Models;
+
+public class SetPlayerPrefsInput : MonoBehaviour {
+
+	// Use this for initialization
+	void Start() {}
+	
+	// Update is called once per frame
+	void Update() {}
+
+    public void SetPlayerName(string name) { PlayerPrefs.SetString("PlayerName", name); }
+        
+    public void SetPlayerID() { 
+        string id = Player.GeneratePlayerID();
+        PlayerPrefs.SetString("PlayerID", id); 
+    }
+    public void SetSinglePlayer() {
+        PlayerPrefs.SetInt("IsPublic", 0);
+        PlayerPrefs.SetInt("NumPlayers", 1);
+    }
+    public void SetMultiPlayer() {
+        PlayerPrefs.SetInt("IsPublic", 1);
+        PlayerPrefs.SetInt("NumPlayers", 2);
+    }
+
+}
