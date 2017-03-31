4196ca18d55e1db26824735d64c68a56dbb9038b
Sam Morrison <morris25@myumanitoba.ca>
2017-03-30 22:50:27 -0500

    Creates a lobby once a side is chosen

--- SingedFeathers/Assets/Scripts/Input/PlayerSideSelectionInput.cs
+++ SingedFeathers/Assets/Scripts/Input/PlayerSideSelectionInput.cs
@@ -1,11 +1,32 @@
 ﻿using CoreGame.Models;
+using Assets.Scripts.Service;
+using CoreGame.Models.API.LobbyClient;
 using UnityEngine;
 
 namespace Assets.Scripts.Input {
     public class PlayerSideSelectionInput : MonoBehaviour {
-        public void ChooseToSavePigeons() { PlayerPrefs.SetInt("Side", (int)PlayerSideSelection.SavePigeons); }
+        public void ChooseToSavePigeons() { 
+            PlayerPrefs.SetInt("Side", (int)PlayerSideSelection.SavePigeons);
+            CreateLobby();
+        }
 
-        public void ChooseToBurnPigeons() { PlayerPrefs.SetInt("Side", (int)PlayerSideSelection.BurnPigeons); }
+        public void ChooseToBurnPigeons() { 
+            PlayerPrefs.SetInt("Side", (int)PlayerSideSelection.BurnPigeons);
+            CreateLobby();
+        }
+
+        private void CreateLobby(){
+            LobbyIO lobbyIO = new LobbyIO();
+            CreateLobbyInfo lobbyInfo = new CreateLobbyInfo();
+            lobbyInfo.HostPlayer = new Player(PlayerPrefs.GetString("PlayerID"), PlayerPrefs.GetString("PlayerID", "AnonPlayer"), (PlayerSideSelection)PlayerPrefs.GetInt("Side"));
+            lobbyInfo.IsPublic = (PlayerPrefs.GetInt("IsPublic") == 1)? true : false;
+            lobbyInfo.LobbyName = PlayerPrefs.GetString("LobbyName", "AnonLobby");
+            lobbyInfo.MapID = PlayerPrefs.GetString("MapID");
+            lobbyInfo.NumPlayers = PlayerPrefs.GetInt("NumPlayers");
+            lobbyIO.CreateLobby(lobbyInfo, delegate(string lobbyID) {
+                PlayerPrefs.SetString("LobbyID", lobbyID);
+            });
+        }
     }
 
 }
