b95dddcc277be9f272a858226e1e7297868d82f9
Garrett Suss <garrettsuss@yext.com>
2017-03-31 17:25:49 -0500

    Fix for createLobby bug

--- SingedFeathers/Assets/Scripts/Input/PlayerSideSelectionInput.cs
+++ SingedFeathers/Assets/Scripts/Input/PlayerSideSelectionInput.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 
 namespace Assets.Scripts.Input {
     public class PlayerSideSelectionInput : MonoBehaviour {
+		LobbyIO _lobbyIO;
+		public GameSelection _gameselect;
+
+		public void Awake() {
+			_lobbyIO = new LobbyIO();
+		}
+
         public void ChooseToSavePigeons() { 
             PlayerPrefs.SetInt("Side", (int)PlayerSideSelection.SavePigeons);
             CreateLobby();
@@ -16,7 +23,6 @@ namespace Assets.Scripts.Input {
         }
 
         private void CreateLobby(){
-            LobbyIO lobbyIO = new LobbyIO();
             CreateLobbyInfo lobbyInfo = new CreateLobbyInfo();
             lobbyInfo.HostPlayer = new Player(PlayerPrefs.GetString("PlayerID"), PlayerPrefs.GetString("PlayerName", "AnonPlayer"), (PlayerSideSelection)PlayerPrefs.GetInt("Side"));
             lobbyInfo.IsPublic = (PlayerPrefs.GetInt("IsPublic") == 1)? true : false;
@@ -24,10 +30,12 @@ namespace Assets.Scripts.Input {
             lobbyInfo.MapID = PlayerPrefs.GetString("MapID");
             lobbyInfo.NumPlayers = PlayerPrefs.GetInt("NumPlayers");
             Debug.Log("Creating Lobby\n" + "Public:"+lobbyInfo.IsPublic + ", "+ lobbyInfo.NumPlayers + "\n"+ lobbyInfo.LobbyName + "\n" + PlayerPrefs.GetString("PlayerID") + "\n" +lobbyInfo.MapID );
-            StartCoroutine(lobbyIO.CreateLobby(lobbyInfo, delegate(string lobbyID) {
+            StartCoroutine(_lobbyIO.CreateLobby(lobbyInfo, delegate(string lobbyID) {
                 PlayerPrefs.SetString("LobbyID", lobbyID);
+
                 Debug.Log(lobbyID == null);
                 Debug.Log(lobbyID);
+				_gameselect.LoadScene("GameScene");
             }));
         }
     }
