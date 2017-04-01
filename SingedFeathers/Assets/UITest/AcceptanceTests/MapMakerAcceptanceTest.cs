ad60472ef15616f0afb2d2807b42e02a405cafe9
radix <hicksonj@myumanitoba.ca>
2017-03-31 22:25:29 -0500

    minor consistency cleanup

---
+++ SingedFeathers/Assets/UITest/AcceptanceTests/MapMakerAcceptanceTest.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+
+namespace Assets.UITest.AcceptanceTests {
+    public class MapMakerAcceptanceTest : global::UITest {
+        [UISetUp]
+        public IEnumerable SetUp()
+        {
+            // Load the scene we want.
+        #if UNITY_EDITOR
+            // The tests are being run through the editor
+            yield return LoadSceneByPath("Assets/Scenes/MapMakerScene.unity");
+        #elif !UNITY_EDITOR
+        // The tests are being run on a device
+            yield return LoadScene("MapMakerScene");
+        #endif
+        }
+
+        [UITest]
+        public IEnumerable TestAppearanceOnLoad() {
+            // Make sure things are active
+            yield return WaitFor(new ObjectAppeared("Main Camera"));
+            yield return WaitFor(new ObjectAppeared("MapMakerView"));
+            yield return WaitFor(new ObjectAppeared("MapMakerCanvas"));
+            yield return WaitFor(new ObjectAppeared("OptionsButton"));
+            yield return WaitFor(new ObjectAppeared("AddFireButton"));
+            yield return WaitFor(new ObjectAppeared("AddPigeonButton"));
+            yield return WaitFor(new ObjectAppeared("UploadButton"));
+            yield return WaitFor(new ObjectAppeared("TileSelectPanel"));
+            yield return WaitFor(new ObjectAppeared("TileSelectList"));
+            yield return WaitFor(new ObjectAppeared("RemoveButton"));
+            yield return WaitFor(new ObjectAppeared("MapMakerText"));
+            yield return WaitFor(new ObjectAppeared("ChangeTileText"));
+            yield return WaitFor(new ObjectAppeared("NumTurnsSlider"));
+            yield return WaitFor(new ObjectAppeared("Background"));
+            yield return WaitFor(new ObjectAppeared("Fill Area"));
+            yield return WaitFor(new ObjectAppeared("Fill"));
+            yield return WaitFor(new ObjectAppeared("Handle Slide Area"));
+            yield return WaitFor(new ObjectAppeared("Handle"));
+            yield return WaitFor(new ObjectAppeared("NumberOfTurnsText"));
+            yield return WaitFor(new ObjectAppeared("NumTurnsValue"));
+            yield return WaitFor(new ObjectAppeared("EventSystem"));
+
+            // Make sure the following is inactive
+            yield return WaitFor(new ObjectDisappeared("MenuCanvas"));
+            yield return WaitFor(new ObjectDisappeared("HelpCanvas"));
+            yield return WaitFor(new ObjectDisappeared("UploadMapCanvas"));
+        }
+
+        [UITest]
+        public IEnumerable TestOptionsButton() {
+            //More detailed tests for the Options Menu is in MapMakerOptionsAcceptanceTest.cs
+            // Access Options
+            yield return Press("OptionsButton");
+
+            // Make sure MenuCanvas and children appeared
+            yield return WaitFor(new ObjectAppeared("MenuCanvas"));
+        }
+
+        [UITest]
+        public IEnumerable TestUploadButton() {
+            // Test only for active elements
+            // Press UploadButton
+            yield return Press("UploadButton");
+
+            // Check that UploadMapCanvas and assumed children are active
+            yield return WaitFor(new ObjectAppeared("UploadMapCanvas"));
+            yield return WaitFor(new ObjectAppeared("MapTitle"));
+            yield return WaitFor(new ObjectAppeared("AuthorNameInput"));
+            yield return WaitFor(new ObjectAppeared("AuthorName"));
+            yield return WaitFor(new ObjectAppeared("MapTitleInput"));
+            yield return WaitFor(new ObjectAppeared("XButton"));
+
+            // ResultText should be inactive
+            yield return WaitFor(new ObjectDisappeared("ResultText"));
+
+            // Press Cancel
+            yield return Press("CancelButton");
+
+            // UploadCanvas should be inactive
+            yield return WaitFor(new ObjectDisappeared("UploadMapCanvas"));
+        }
+
+    }
+}
---
+++ SingedFeathers/Assets/UITest/AcceptanceTests/MapMakerOptionsAcceptanceTest.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+
+namespace Assets.UITest.AcceptanceTests {
+    public class MapMakerOptionsAcceptanceTest : global::UITest {
+        [UISetUp]
+        public IEnumerable SetUp()
+        {
+            // Load the scene we want.
+        #if UNITY_EDITOR
+            // The tests are being run through the editor
+            yield return LoadSceneByPath("Assets/Scenes/MapMakerScene.unity");
+        #elif !UNITY_EDITOR
+        // The tests are being run on a device
+            yield return LoadScene("MapMakerScene");
+        #endif
+        }
+
+        [UITest]
+        public IEnumerable TestAppearance() {
+            // Access Options
+            yield return Press("OptionsButton");
+
+            // Make sure MenuCanvas and children appeared
+            yield return WaitFor(new ObjectAppeared("MenuCanvas"));
+            yield return WaitFor(new ObjectAppeared("Panel"));
+            yield return WaitFor(new ObjectAppeared("RestartButton"));
+            yield return WaitFor(new ObjectAppeared("ReturnToTitleButton"));
+            yield return WaitFor(new ObjectAppeared("OptionsText"));
+            yield return WaitFor(new ObjectAppeared("HowToButton"));
+        }
+
+        [UITest]
+        public IEnumerable TestResumeButton() {
+            // Access Options
+            yield return Press("OptionsButton");
+
+            // return to map editing
+            yield return Press("ResumeButton");
+
+            // Make sure the MenuCanvas and assumed children are inactive
+            yield return WaitFor(new ObjectDisappeared("MenuCanvas"));
+            yield return WaitFor(new ObjectDisappeared("Panel"));
+            yield return WaitFor(new ObjectDisappeared("RestartButton"));
+            yield return WaitFor(new ObjectDisappeared("ReturnToTitleButton"));
+            yield return WaitFor(new ObjectDisappeared("OptionsText"));
+            yield return WaitFor(new ObjectDisappeared("HowToButton"));
+        }
+
+        [UITest]
+        public IEnumerable TestReturnToTitleButton() {
+            // Access Options
+            yield return Press("OptionsButton");
+
+            // Return to title
+            yield return Press("ReturnToTitleButton");
+
+            // Make sure it worked!
+            yield return WaitFor(new SceneLoaded("GameSelectScene"));
+        }
+
+        [UITest]
+        public IEnumerable TestHowToButton() {
+            // Access Options
+            yield return Press("OptionsButton");
+
+            // Access How to play canvas
+            yield return Press("HowToButton");
+
+            // Make sure HelpCanvas and children are active
+            yield return WaitFor(new ObjectAppeared("HelpCanvas"));
+            yield return WaitFor(new ObjectAppeared("Panel"));
+            yield return WaitFor(new ObjectAppeared("HelpTitle"));
+            yield return WaitFor(new ObjectAppeared("HelpBodyText"));
+
+            // Other UI elements should be inactive
+            yield return WaitFor(new ObjectDisappeared("MapMakerCanvas"));
+            yield return WaitFor(new ObjectDisappeared("MenuCanvas"));
+
+            // Go back to options
+            yield return Press("Button");
+
+            // Help canvas and assumed childre should be inactive
+            // Make sure HelpCanvas and children are active
+            yield return WaitFor(new ObjectDisappeared("HelpCanvas"));
+            yield return WaitFor(new ObjectDisappeared("HelpTitle"));
+            yield return WaitFor(new ObjectDisappeared("HelpBodyText"));
+
+            // MeunuCanvas should be active again
+            yield return WaitFor(new ObjectAppeared("MenuCanvas"));
+
+            // MapMakerCanvas should still be incative
+            yield return WaitFor(new ObjectDisappeared("MapMakerCanvas"));
+        }
+    }
+}
