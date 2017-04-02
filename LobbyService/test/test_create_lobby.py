e8754050040267c973681d94aebb8f77fd0ad5f8
devin0xffffff <devin0xffffff@gmail.com>
2017-04-02 12:38:23 -0500

    Quick fixes for LobbyUI development

--- LobbyService/test/test_create_lobby.py
+++ LobbyService/test/test_create_lobby.py
@@ -10,18 +10,32 @@ valid_event_body = {
     }
 
 
+@patch('service.lobby_service_common.get_map_name')
 @patch('service.lobby_service_common.create_lobby')
-def test_create_map(create_lobby_mock):
+def test_create_lobby(create_lobby_mock, get_map_name_mock):
+    get_map_name_mock.return_value = ''
+
     response = create_lobby.lambda_handler(valid_event_body, None)
 
     create_lobby_mock.assert_called()
+    get_map_name_mock.assert_called()
 
-    assert response['statusCode'] == 200 and "LobbyID" in response['body']
+    assert response['statusCode'] == 200
 
 
-@patch('service.lobby_service_common.create_lobby')
-def test_get_maps_dynamo_failure(create_lobby_mock):
+@patch('service.lobby_service_common.get_map_name')
+def test_get_map_name_dynamo_failure(get_map_name_mock):
     with pytest.raises(IOError):
+        get_map_name_mock.side_effect = Mock(side_effect=IOError('Dynamo Exception'))
+
+        create_lobby.lambda_handler(valid_event_body, None)
+
+
+@patch('service.lobby_service_common.get_map_name')
+@patch('service.lobby_service_common.create_lobby')
+def test_create_lobby_dynamo_failure(create_lobby_mock, get_map_name_mock):
+    with pytest.raises(IOError):
+        get_map_name_mock.return_value = ''
         create_lobby_mock.side_effect = Mock(side_effect=IOError('Dynamo Exception'))
 
         create_lobby.lambda_handler(valid_event_body, None)
